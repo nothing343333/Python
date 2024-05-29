d3c15031fb3fc090666a90514bb3c10d861eede3
Tirth S Pandya <166584587+Flintiee@users.noreply.github.com>
2024-05-29 21:32:43 +0530

    commit

---
+++ Level-1/StringsInDetail.py
@@ -0,0 +1,71 @@
+# str.upper() and str.lower()
+# text = "hello, world"
+
+# upper_text = text.upper()
+# lower_text = text.lower()
+
+# print(upper_text)
+# print(lower_text)
+
+
+# str.capitalize() and str.title()
+
+# phrase = "Python programing is so fun"
+# cap_phrase = phrase.capitalize()
+# print(cap_phrase)
+# tit_phrase = phrase.title()
+# print(tit_phrase)
+
+
+
+
+
+
+# str.strip(), str.lstrip(), str.rstrip()
+
+# sentence = "    Python is fun!!    "
+# stripped_sentence = sentence.strip()
+# print(stripped_sentence)
+# removes spaces from right and left side
+# ls will remove from left side and vica versa
+
+
+
+
+
+
+# str.starwith(prefix) and str.endswith(suffix)
+# filename = "Example.txt"
+# starts_with = filename.startswith("Example")
+# print(starts_with)
+# ends_with = filename.endswith(".txt")
+# print(ends_with)
+# doesNotEnd_with = filename.endswith("lol")
+# print(doesNotEnd_with)
+
+# str.replace("old", "new")
+# sentence = "I like to move up like a star"
+# new_sentence = sentence.replace("star", "god")
+# print(new_sentence)
+# index1 = sentence.find("like")
+# print(index1)
+# index2 = sentence.index("like")
+# print(index2)
+
+
+
+
+# str.count(target)
+# sentence = "Kedaar is a good boi, Kedaar is a sweet boi, Kedaar is very handsome, Kedaar is awesome"
+# counted = sentence.count("Kedaar")
+# print(counted)
+
+
+
+
+# str.split(separator)
+# sentence = "Python is fun yaar and deep"
+# words = sentence.split("Python")
+# print(words)
+
+
--- Level-1/strupperordown.py
+++
@@ -1,8 +0,0 @@
-# str.upper() and str.lower()
-text = "hello, world"
-
-upper_text = text.upper()
-lower_text = text.lower()
-
-print(upper_text)
-print(lower_text)
