1cfcd33c562d78e3c7ccfbf1fd6890d753c660b9
Tirth S Pandya <166584587+Flintiee@users.noreply.github.com>
2024-05-29 22:33:34 +0530

    commit

---
+++ Level-1/CommonOprators.py
@@ -0,0 +1,36 @@
+# a = 5
+# b = 10
+
+# a == b
+
+# print(a==b) false
+# print(a>b) false
+# print(a<b) true
+
+
+# result = a!=b
+# print(result)
+
+
+# baaki to kr lega dost :)
+
+
+
+# AND, OR, NOT,
+# print(True or True)
+# will see only one condition right
+
+
+# print(True and False){
+# will check both the values}
+
+
+# print( not False){
+# not equal to }
+# matlab true print krega
+
+
+
+
+
+
---
+++ Level-1/loops.py
@@ -0,0 +1,37 @@
+# if, else, elif
+
+# x = 0
+
+# if x > 0:
+#     print("Shit!")
+
+# elif x == 0:
+#     print("True")
+
+# else:
+#     print("False dumb")
+
+
+
+# for loop
+
+# for i in range(5):
+#     # print(i)
+#     print("My name is not HuXn", i)
+
+
+
+
+
+# for i in range(start, stop, skip)
+# for i in range(1, 10, 2):
+#     print(i)
+    # 1 se start hoga 10 tak aur 2 aur uske multiple skip hongw
+
+
+
+
+word = "Lol"
+
+for char in word:
+    print(char)
