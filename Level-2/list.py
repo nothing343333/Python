044bfe468d46595f47d790fb43603f233d1582ae
Tirth S Pandya <166584587+Flintiee@users.noreply.github.com>
2024-05-30 11:16:32 +0530

    commit

---
+++ Level-2/list.py
@@ -0,0 +1,126 @@
+# thoda dhyan se krna bhai intermediate level hai
+
+# creating a list of numbers
+
+# numbers = [1,2,3,4,5]
+# print(len(numbers))
+# print(numbers)
+
+
+
+
+# creating a list of strings
+# fruits = ["apple", "banana", "dorange", "orange", "grapes"]
+# print(fruits)
+# print(fruits[2])
+
+
+
+
+
+# creating a mixed-type list
+# Mixed_list = [1, "Hello", 3.14, "Gello"] 
+# print(Mixed_list)
+# print(len(Mixed_list))
+
+
+
+
+
+
+# Acessing the items from the list
+# list = [1,2,3,4,5,6]
+# print(list)
+# print(list[0])
+# print(list[1])
+# print(list[2])
+# print(list[3])
+# print(list[-1]) from last
+# print(list[-2]) from second last
+
+# slices!
+# subset = list[1:3]
+# will start from index 1 and willl stop at index 2 not 3.
+# print(subset)
+# subset = list[-3:]
+# will print from last to third last
+# print(subset)
+
+
+# updating items
+# mutable = change
+# immutable = cannot change
+
+
+# list = [1,2,3,4,5]
+# select the element using index
+# assign it a new value because a list is mutable
+# stable = list[0] = 65
+# print(stable)
+
+
+
+
+
+#using del
+# lol_list = [1,2,3,4]
+# lol_list = list.remove(3)
+# print(lol_list)
+
+
+
+
+# 1D, 2D, 3D
+# 2D
+# matrix = [[1,2,3], [4,5,6], [7,8,9,10]]
+# print(matrix)
+# print(matrix[0])
+# print(matrix[1])
+# print(matrix[2])
+
+# 3D
+# cube = [
+#     [
+#      [1,2,3],
+#      [4,5,6],
+#      [7,8]
+#     ]
+
+#     [
+#      [67,43,23],
+#      [42,35,56],
+#      [71,28]
+#     ]
+
+#     [
+#      [1054,232,2123],
+#      [443,534,6212],
+#      [721,80,5454]
+#     ]
+# ]
+# print(cube[0][0])
+# print(cube[1][0])
+# print(cube[2][1])
+
+
+# append and extend
+# append
+# list = [1,2,3]
+# list = list.append(5)
+# print(list)
+
+
+
+
+# extend 
+# list = [1,2,3]
+# list = list.extend([4,5,6])
+# print(list)
+
+
+# remove
+# list = [1,2,3]
+# list.remove(1)
+# print(list)
+# or
+# list.pop(1)
