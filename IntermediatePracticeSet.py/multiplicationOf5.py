b10255646472997cd55ba0cefd136b223af4290f
Tirth S Pandya <teamfznltd@gmail.com>
2024-05-31 16:03:09 +0000

    commit

---
+++ IntermediatePracticeSet.py/multiplicationOf5.py
@@ -0,0 +1,3 @@
+for i in range(10):
+    print(f"5x{i+1}=",5*(i+1))
+    i = i+1
---
+++ IntermediatePracticeSet.py/summationOfValue.py
@@ -0,0 +1,6 @@
+# def summation(n):
+#     return 1 if (n == 0 or n == 1) else n + summation(n-1)
+
+# num = 5
+# print("the summation of", sum, "is", summation(num))
+
