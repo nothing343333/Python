42f582bd27069b673914f1ffa21c6d74af153769
Tirth S Pandya <166584587+Flintiee@users.noreply.github.com>
2024-06-01 17:09:14 +0530

    commit

---
+++ Level-2/scope.py
@@ -0,0 +1,35 @@
+# global_variable = 10  # This is a global variable
+
+# def print_global_variable():
+#     print(global_variable)
+
+# print_global_variable()  # Outputs: 10
+
+
+
+
+# # def my_func():
+#     local_variable = 5  # This is a local variable
+#     print(local_variable)
+
+# my_func()  # Outputs: 5
+
+# Attempting to access the local variable outside the function would result in an error
+# print(local_variable)  # Raises a NameError
+
+
+
+
+
+# def outer_function():
+#     outer_variable = "I'm in outer function"
+
+#     def inner_function():
+#         print(outer_variable)
+
+#     inner_function()
+
+# outer_function()  # Outputs: I'm in outer function
+
+
+
