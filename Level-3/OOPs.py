89b1c8fecf985cc1e1be0af736b9f1a939e5d20d
Tirth S Pandya <166584587+Flintiee@users.noreply.github.com>
2024-06-20 16:22:16 +0530

    commit

---
+++ Level-3/OOPs.py
@@ -0,0 +1,8 @@
+# POP Produce oreinted programming 
+# functions and definations use hote hai not objects 
+
+# OOPs object oreinted programming
+# classes and objects use hote hai not functions
+# is a programming paroday where the software desgin revolves around objects or data rather than functions.
+
+# why use OOPs?
