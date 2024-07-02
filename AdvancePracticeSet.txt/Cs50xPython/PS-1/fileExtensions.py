977baa2e83cd5587ad8f593009c54feb7dfe93a2
Tirth S Pandya <166584587+Flintiee@users.noreply.github.com>
2024-07-02 11:00:38 +0530

    commit

--- AdvancePracticeSet.txt/Cs50xPython/PS-1/fileExtensions.py
+++ AdvancePracticeSet.txt/Cs50xPython/PS-1/fileExtensions.py
@@ -1,4 +1,19 @@
 name = input("File Name: ")
-if ".jpg" in name or ".gif" in name or "jpeg" in name:
-    result = name.replace("jpg", "/jpg")
-    print(result)
+if ".jpg" in name:
+    result = name.replace(".jpg", "  ")
+    final = result.replace(result, " image/jpg")
+    print(final)
+elif ".png" in name:
+    result = name.replace(".png", "  ")
+    final = result.replace(result, " image/png")
+    print(final)
+elif ".jpg" in name:
+    result = name.replace(".gif", "  ")
+    final = result.replace(result, " image/gif")
+    print(final)
+    # result = name.replace(".gif", "  ")
+    # final = result.replace(result, " image/gif")
+    # print(final)
+    # result = name.replace(".jpeg", "  ")
+    # final = result.replace(result, " image/jpeg")
+    # print(final)
