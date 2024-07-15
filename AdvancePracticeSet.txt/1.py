bf400e02b5d0c685d3d6f318d511034cd03dc7e7
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-15 13:15:40 +0530

    commit

---
+++ AdvancePracticeSet.txt/1.py
@@ -0,0 +1,16 @@
+# Create a class “Programmer” for storing information of few programmers
+# working at Microsoft.
+class Programmer:
+    salary = 150000
+    emp_id = 13202
+    name = "Go go"
+    company = "Microsoft"
+
+daman = Programmer()
+Programmer.salary = 120000
+Programmer.emp_id = 1
+Programmer.name = "Daman"
+print(Programmer.name)
+print(Programmer.emp_id)
+print(Programmer.salary)
+# print(Programmer.)
