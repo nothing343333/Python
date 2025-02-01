0999d66dd2c715b03b252c54d8b65fbb6d7d2724
nothing343333 <tpandya779@gmail.com>
2025-02-01 11:32:09 +0000

    .

--- PlayGround/index.py
+++ PlayGround/index.py
@@ -1,4 +1,1 @@
-import numpy as np
-arr = np.array([1,2,3,5,5])
-arr2 = np.array([32,23,21,12])
-print(np.sum([arr, arr2]))
+print("This is just for playing around the code, nothing to change here\n")
